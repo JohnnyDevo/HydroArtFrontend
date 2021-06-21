f0708ed39255617354a8171e930ada14cd24afbe
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-06-21 19:56:34 -0400

    run this after loading the page

--- js/get-specific-card-info.js
+++ js/get-specific-card-info.js
@@ -1,11 +1,13 @@
-let paramName = 'card';
-
-//regex to read the parameters
-paramName = paramName.replace(/[\[\]]/g, '\\$&');
-const regex = new RegExp('[?&]' + paramName + '(=([^&#]*)|&|#|$)');
-const results = regex.exec(window.location.href);
-
-if (results && results[2]) {
-    const element = document.getElementById('result');
-    element.innerHTML = `The card searched for is "${decodeURIComponent(results[2].replace(/\+/g, ' '))}".`;
+document.onload = () => {
+    let paramName = 'card';
+    
+    //regex to read the parameters
+    paramName = paramName.replace(/[\[\]]/g, '\\$&');
+    const regex = new RegExp('[?&]' + paramName + '(=([^&#]*)|&|#|$)');
+    const results = regex.exec(window.location.href);
+    
+    if (results && results[2]) {
+        const element = document.getElementById('result');
+        element.innerHTML = `The card searched for is "${decodeURIComponent(results[2].replace(/\+/g, ' '))}".`;
+    }
 }
