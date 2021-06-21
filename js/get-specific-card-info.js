5981838a2a10e00a70396a85f0b3e0e9d7fee288
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-06-21 19:58:52 -0400

    logging!

--- js/get-specific-card-info.js
+++ js/get-specific-card-info.js
@@ -1,13 +1,16 @@
+window.alert("js reached");
 document.onload = () => {
     let paramName = 'card';
-    
+
     //regex to read the parameters
     paramName = paramName.replace(/[\[\]]/g, '\\$&');
     const regex = new RegExp('[?&]' + paramName + '(=([^&#]*)|&|#|$)');
     const results = regex.exec(window.location.href);
-    
+    window.alert("regex happened");
+
     if (results && results[2]) {
         const element = document.getElementById('result');
         element.innerHTML = `The card searched for is "${decodeURIComponent(results[2].replace(/\+/g, ' '))}".`;
+        window.alert(results);
     }
 }
