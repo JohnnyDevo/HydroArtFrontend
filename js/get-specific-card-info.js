b1189958ff554986aa46b33ac895d89656a7d0ca
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-06-21 20:09:06 -0400

    get rid of test alerts

--- js/get-specific-card-info.js
+++ js/get-specific-card-info.js
@@ -1,16 +1,13 @@
 function getCardName() {
     let paramName = 'card'
-    
+
     //regex to read the parameters
-    window.alert("before regex");
     paramName = paramName.replace(/[\[\]]/g, '\\$&');
     const regex = new RegExp('[?&]' + paramName + '(=([^&#]*)|&|#|$)');
     const results = regex.exec(window.location.href);
-    window.alert("regex happened")
-    
+
     if (results && results[2]) {
         const element = document.getElementById('result');
-        element.innerHTML = `The card searched for is "${decodeURIComponent(results[2].replace(/\+/g, ' '))}".`;
-        window.alert(results);
+        element.innerText = `The card searched for is "${decodeURIComponent(results[2].replace(/\+/g, ' '))}".`;
     }
 }
