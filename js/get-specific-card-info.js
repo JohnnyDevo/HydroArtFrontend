7f6a2d8d62d3bb615221c170ff3d33b0514235d1
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-06-21 20:07:30 -0400

    onload attempt #2

--- js/get-specific-card-info.js
+++ js/get-specific-card-info.js
@@ -1,14 +1,13 @@
-window.alert("js reached");
-document.onload = () => {
-    let paramName = 'card';
-
+function getCardName() {
+    let paramName = 'card'
+    
     //regex to read the parameters
     window.alert("before regex");
     paramName = paramName.replace(/[\[\]]/g, '\\$&');
     const regex = new RegExp('[?&]' + paramName + '(=([^&#]*)|&|#|$)');
     const results = regex.exec(window.location.href);
-    window.alert("regex happened");
-
+    window.alert("regex happened")
+    
     if (results && results[2]) {
         const element = document.getElementById('result');
         element.innerHTML = `The card searched for is "${decodeURIComponent(results[2].replace(/\+/g, ' '))}".`;
