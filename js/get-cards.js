4c7ff8034bb5afbc547ebaea0dcbe1eea30c2923
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-06-27 00:06:29 -0400

    automatically populate results with card images

--- js/get-cards.js
+++ js/get-cards.js
@@ -10,9 +10,8 @@ async function getAllCards() {
         referrerPolicy: 'no-referrer'
     });
 
-    const result = await response.json();
-
-    document.getElementById('results').innerText = JSON.stringify(result, null, 1);
+    const cards = await response.json();
+    populateResults(cards);
 }
 
 function getSearchText() {
@@ -46,7 +45,87 @@ async function doSearch() {
         referrerPolicy: 'no-referrer'
     });
 
-    const result = await response.json();
+    const cards = await response.json();
+    populateResults(cards);
+}
 
-    document.getElementById('results').innerText = JSON.stringify(result, null, 1);
+function populateResults(cards) {
+    const resultsElement = document.getElementById('cards-container');
+    for (const card in cards) {
+        resultsElement.appendChild(makeCard(card));
+    }
+}
+
+function makeCard(card) {
+    const cardElement = document.createElement("div");
+    cardElement.className = "card";
+    if (!card.subtype) {
+        card.subtype = "";
+    } else {
+        card.subtype = `/${card.subtype}`;
+    }
+    if (card.cost == -1) {
+        card.cost = "X";
+    }
+
+    let url = `/portraits/${card.type}.png`;
+    const portraitElement = createImageElement("portrait", url);
+    cardElement.appendChild(portraitElement);
+    
+    if (card.subtype) {
+        url = `/portraits${card.subtype}/${card.type}_desc.png`;
+        const descElement = createImageElement("description-background", url);
+        cardElement.appendChild(descElement);
+    }
+
+    url = `/portraits${card.subtype}/${card.type}_frame.png`;
+    const frameElement = createImageElement("frame", url);
+    cardElement.appendChild(frameElement);
+    
+    url = `/portraits/${card.rarity}_banner.png`
+    const bannerElement = createImageElement("banner", url);
+    cardElement.appendChild(bannerElement);
+    
+    if (card.cost > -2) {
+        url = `/portraits${card.subtype}/energy.png`;
+        const energyElement = createImageElement("energy-symbol", url)
+        cardElement.appendChild(energyElement);
+    }
+
+    const costTextContainer = createDescriptionElement("cost", card.cost);
+    cardElement.appendChild(costTextContainer);
+
+    const upgradedCostTextContainer = createDescriptionElement("cost", card.upgraded_cost);
+    cardElement.appendChild(upgradedCostTextContainer);
+
+    const nameTextContainer = createDescriptionElement("name", card.name);
+    cardElement.appendChild(nameTextContainer);
+
+    const typeTextContainer = createDescriptionElement("type", card.type);
+    cardElement.appendChild(typeTextContainer);
+
+    const descriptionTextContainer = createDescriptionElement("description", card.description);
+    cardElement.appendChild(descriptionTextContainer);
+
+    const upgradedDescriptionTextContainer = createDescriptionElement("description", card.upgraded_description);
+    cardElement.appendChild(upgradedDescriptionTextContainer);
+
+    return cardElement;
+}
+
+function createImageElement(type, path) {
+    const element = document.createElement("img");
+    element.className = `card-${type}`;
+    element.src = path;
+    return element;
+}
+
+function createDescriptionElement(type, content) {
+    const container = document.createElement("div");
+    container.className = `card-${type}-container`;
+    const text = document.createElement("div");
+    text.className = `${type}-text`;
+    text.innerHTML = content;
+    container.appendChild(text);
+    return container;
 }
