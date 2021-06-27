feecebf5e140112e431fb1ff30237e89d0ddf89e
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-06-26 20:05:32 -0400

    combine card retrieval into a single script file

--- js/get-cards.js
+++ js/get-cards.js
@@ -1,3 +1,20 @@
+async function getAllCards() {
+    const destination = 'http://localhost:3000/cards';
+
+    const response = await fetch(destination, {
+        method: 'GET',
+        redirect: 'manual',
+        headers: {
+            'Content-Type': 'application/json;charset=UTF-8'
+        },
+        referrerPolicy: 'no-referrer'
+    });
+
+    const result = await response.json();
+
+    document.getElementById('results').innerText = JSON.stringify(result, null, 1);
+}
+
 function getSearchText() {
     let paramName = 'search'
 
