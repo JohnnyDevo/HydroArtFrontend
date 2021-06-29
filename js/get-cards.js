0a7af03e6aebd05c5bce555f810286a16066f184
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-06-29 00:07:07 -0400

    start up "single card view" interface

--- js/get-cards.js
+++ js/get-cards.js
@@ -14,8 +14,7 @@ async function getAllCards() {
     populateResults(result.cards, result.arts);
 }
 
-function getSearchText() {
-    let paramName = 'search'
+function getQueryText(paramName) {
 
     //regex to read the parameters
     paramName = paramName.replace(/[\[\]]/g, '\\$&');
@@ -28,13 +27,13 @@ function getSearchText() {
 };
 
 async function doSearch() {
-    const search = getSearchText();
+    const search = getQueryText('search');
     if (!search) {
-        search = "";
+        return window.location.replace('/cards/index.html');
     }
     document.getElementById('search-text').innerText = `you searched for: ${search}`;
     const destination = 'http://localhost:3000/cards/search?';
-    const query = `search=${encodeURIComponent(search)}`
+    const query = `search=${encodeURIComponent(search)}`;
 
     const response = await fetch(`${destination}${query}`, {
         method: 'GET',
@@ -49,6 +48,26 @@ async function doSearch() {
     populateResults(result.cards, result.arts);
 }
 
+async function getSingleCard() {
+    const cardID = getQueryText('cardID');
+    if (!cardID) {
+        return window.location.replace('/cards/index.html');
+    }
+    const destination = `http://localhost:3000/cards/${cardID}`;
+
+    const response = await fetch(`${destination}${query}`, {
+        method: 'GET',
+        redirect: 'manual',
+        headers: {
+            'Content-Type': 'application/json;charset=UTF-8'
+        },
+        referrerPolicy: 'no-referrer'
+    });
+
+    const result = await response.json();
+    makeSingleCardView(result);
+}
+
 function populateResults(cards, arts) {
     const resultsElement = document.getElementById('cards-container');
     for (const card in cards) {
@@ -56,13 +75,17 @@ function populateResults(cards, arts) {
         if (arts) {
             art = arts.filter(art => {return art.card_id === cards[card].id})[0];
         }
-        resultsElement.appendChild(makeCard(cards[card], art));
+        resultsElement.appendChild(makeCard(cards[card], art, false));
     }
 }
 
-function makeCard(card, art) {
+function makeCard(card, art, preview) {
     const cardElement = document.createElement("div");
-    cardElement.className = "card";
+    if (preview) {
+        cardElement.className = "card-preview";
+    } else {
+        cardElement.className = "card";
+    }
     if (!card.subtype) {
         card.subtype = "";
     } else {
@@ -147,4 +170,83 @@ function createDescriptionElement(type, content) {
     text.innerHTML = content;
     container.appendChild(text);
     return container;
+}
+
+const artsArray = [];
+
+function changeArt(next) {
+    for (let i = 0; i < artsArray.length; ++i) {
+        if (artsArray[i].style.display === 'block') {
+            artsArray[i].style.display = 'none';
+            let nextArt = i;
+            if (next) {
+                nextArt++;
+            } else {
+                nextArt--;
+            }
+            if (nextArt < 0) {
+                nextArt = artsArray.length - 1;
+            }
+            if (nextArt >= artsArray.length) {
+                nextArt = 0;
+            }
+            artsArray[nextArt].style.display === 'block';
+            break;
+        }
+    }
+}
+
+function showNextArt() {
+    changeArt(true);
+}
+
+function showPreviousArt() {
+    changeArt(false);
+}
+
+function makeSingleCardView(cardInfo) {
+
+    const resultsElement = document.getElementById('cards-container');
+    const card = makeCard(cardInfo.card, null, false);
+    card.style.setProperty('--size-multiplier', "calc(2 * var(--size-multiplier))");
+    resultsElement.appendChild(card);
+
+    if (cardInfo.arts) {
+        for (const artInfo in cardInfo.arts) {
+            let url = `data:image/png;base64,${cardInfo.arts[artInfo].encode}`;
+            const artElement = createImageElement("art", url);
+            card.appendChild(artElement);
+            artsArray.push[artElement];
+            if (artInfo.id !== cardInfo.defaultArtID) {
+                artElement.style.display = "none";
+            }
+        }
+        if (artsArray.length > 1) {
+            const showPreviousArt = document.createElement("button");
+            showPreviousArt.id = "show-previous-art";
+            showPreviousArt.onclick = showPreviousArt;
+            const showNextArt = document.createElement("button");
+            showNextArt.id = "show-next-art";
+            showNextArt.onclick = showNextArt;
+            resultsElement.prepend(showPreviousArt);
+            resultsElement.append(showNextArt);
+        }
+    }
+
+    if (cardInfo.keywords) {
+        //create a keywords container
+        for (const keywordInfo in cardInfo.keywords) {
+            //create a keyword, append it to keywords container
+        }
+        //append keywords container to resultsElement
+    }
+
+    if (cardInfo.card.swapsTo) {
+        let art;
+        if (cardInfo.card.swapsTo.art) {
+            art = cardInfo.card.swapsTo.art;
+        }
+        const previewElement = makeCard(cardInfo.card.swapsTo, art, true);
+        resultsElement.prepend(previewElement);
+    }
 }
