12ec8c88aaddb80f231ad9baaa3b4c4726467e8b
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-07-03 15:49:43 -0400

    Turn retrieved data into art placards

--- js/get-art.js
+++ js/get-art.js
@@ -13,11 +13,75 @@ async function getAllArt() {
     
         if (response.ok) {
             const result = await response.json();
-            console.log(result);
+            populateArt(result);
         } else {
             console.log(response);
         }
     } catch (error) {
         //render something in the browser to let the user know something happened
     }
+}
+
+function populateArt(results) {
+    //if no art, do something in the browser to show that there is no art (yet! :D)
+    const container = document.getElementById("art-container");
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    for (const artIndex in results) {
+        const art = results[artIndex];
+        const artElement = makeArtElement(art);
+        container.appendChild(artElement);
+    }
+}
+
+function makeArtElement(art) {
+    const buffer = document.createElement("div");
+    buffer.className = "art-buffer";
+
+    const info = document.createElement("div");
+    info.className = "art-info";
+    buffer.appendChild(info);
+
+    const imageContainer = document.createElement("div");
+    imageContainer.className = "art-image-container";
+    info.appendChild(imageContainer);
+
+    const image = document.createElement("img");
+    image.className = "art-image";
+    image.src = `data:image/png;base64,${art.encode}`;
+    imageContainer.appendChild(image);
+
+    const infoContainer = document.createElement("div");
+    infoContainer.className = "artist-info-container";
+    info.appendChild(infoContainer);
+
+    const artistLink = document.createElement("a");
+    artistLink.href = `/art/artists/index.html?artistID=${art.user_id}`
+    artistLink.className = "artist-view-link";
+    artistLink.innerText = art.credits_name;
+    infoContainer.appendChild(artistLink);
+
+    const artistPage = document.createElement("a");
+    artistPage.href = art.credits_url;
+    artistPage.className = "artist-page-link";
+    artistPage.innerText = art.credits_url;
+    infoContainer.appendChild(artistPage);
+
+    const cardLinkContainer = document.createElement("div");
+    cardLinkContainer.className = "art-card-link";
+    infoContainer.appendChild(cardLinkContainer);
+
+    const cardIcon = document.createElement("img");
+    cardIcon.className = "art-card-icon";
+    cardIcon.src = "/card_icon.png";
+    cardLinkContainer.appendChild(cardIcon);
+
+    const cardName = document.createElement("a");
+    cardName.href = `/cards/view/index.html?cardID=${art.card_id}`;
+    cardName.classname = "art-card-name";
+    cardName.innerText = art.card_name;
+    cardLinkContainer.appendChild(cardName);
+
+    return buffer;
 }
