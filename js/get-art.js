294399d5885081096f9d85a1194a80deb7967cc6
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-07-03 20:51:27 -0400

    users can now filter their art searches by card ID and user ID

--- js/get-art.js
+++ js/get-art.js
@@ -22,6 +22,54 @@ async function getAllArt() {
     }
 }
 
+function getQueryText(paramName) {
+
+    //regex to read the parameters
+    paramName = paramName.replace(/[\[\]]/g, '\\$&');
+    const regex = new RegExp('[?&]' + paramName + '(=([^&#]*)|&|#|$)');
+    const results = regex.exec(window.location.href);
+
+    if (results && results[2]) {
+        return decodeURIComponent(results[2].replace(/\+/g, ' '));
+    }
+};
+
+async function getArt() {
+    const card = getQueryText("cardID");
+    const artist = getQueryText("artistID");
+    if (!card && !artist) {
+        return window.location.replace('/art/index.html');
+    }
+
+    let destination = `${API_URL}:${API_PORT}/art`;
+    if (card) {
+        destination += `/cards/${card}`;
+    }
+    if (artist) {
+        destination += `/users/${artist}`;
+    }
+
+    try {
+        const response = await fetch(destination, {
+            method: 'GET',
+            redirect: 'manual',
+            headers: {
+                'Content-Type': 'application/json;charset=UTF-8'
+            },
+            referrerPolicy: 'no-referrer'
+        });
+    
+        if (response.ok) {
+            const result = await response.json();
+            populateArt(result);
+        } else {
+            console.log(response);
+        }
+    } catch (error) {
+        //render something in the browser to let the user know something happened
+    }
+}
+
 function populateArt(results) {
     //if no art, do something in the browser to show that there is no art (yet! :D)
     const container = document.getElementById("art-container");
