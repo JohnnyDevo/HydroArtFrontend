fd50610a40b55ed28f9b6c51e418e3ebcb06fef9
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-07-05 20:20:50 -0400

    moved upload to its own page

---
+++ js/get-card-list.js
@@ -0,0 +1,52 @@
+function getQueryText(paramName) {
+
+    //regex to read the parameters
+    paramName = paramName.replace(/[\[\]]/g, '\\$&');
+    const regex = new RegExp('[?&]' + paramName + '(=([^&#]*)|&|#|$)');
+    const results = regex.exec(window.location.href);
+
+    if (results && results[2]) {
+        return decodeURIComponent(results[2].replace(/\+/g, ' '));
+    }
+};
+
+async function getAllCards() {
+    const destination = `${API_URL}:${API_PORT}/cards/names`;
+
+    try {
+        const response = await fetch(destination, {
+            method: 'GET',
+            redirect: 'manual',
+            headers: {
+                'Content-Type': 'application/json;charset=UTF-8'
+            },
+            referrerPolicy: 'no-referrer'
+        });
+    
+        if (response.ok) {
+            const result = await response.json();
+            populateOptions(result);
+        } else {
+            //render the response's error
+        }
+    } catch (error) {
+        //render something in the browser to let the user know something happened
+    }
+}
+
+function populateOptions(results) {
+    const cardID = getQueryText("cardID");
+    const selector = document.getElementById("card-id-selector");
+
+    for (const index in results) {
+        const card = results[index];
+
+        const option = document.createElement("option");
+        option.value = card.id;
+        option.innerText = card.name;
+
+        if (cardID == card.id) {
+            selector.value = card.id;
+        }
+    }
+}
