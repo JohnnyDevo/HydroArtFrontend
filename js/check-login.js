a65f8f46b0f51ba7c3aea383724f8959f26dd8e3
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-07-06 23:24:42 -0400

    implement a sign-in prompt, replace the sign-in page with it

--- js/check-login.js
+++ js/check-login.js
@@ -1,7 +1,10 @@
-async function onLoadPage(redirect) {
+window.onload = onLoadPage;
+
+async function onLoadPage(askLogin) {
     const destination = `${API_URL}:${API_PORT}/users`;
 
     let username;
+    let ok = false;
     try {
         const response = await fetch(destination, {
             method: 'GET',
@@ -16,18 +19,132 @@ async function onLoadPage(redirect) {
         if (response.ok) {
             const user = await response.json();
             username = user.username;
-            if (!user.username) {
-                if (redirect) {
-                    window.location.replace('/signin');
-                }
+            if (user.username) {
+                ok = true;
             }
         }
     } catch (error) {
         //render something in the browser to let the user know something happened
     }
 
+    if (!ok && askLogin) {
+        makeSigninPrompt(false);
+    }
+
     makeHeader(username);
     makeFooter();
+}
+
+function makeSigninPrompt(skipObserve) {
+    const html = document.getElementsByTagName("html")[0];
+
+    const film = document.createElement("div");
+    film.id = "login-tint";
+    html.appendChild(film);
+
+    if (!skipObserve) {
+        const observer = new MutationObserver(mutations => {
+            mutations.forEach(mutation => {
+                mutation.removedNodes.forEach(node => {
+                    if (node.id == "login-tint") {
+                        history.back();
+                        observer.disconnect();
+                    }
+                });
+            });
+        });
+
+        observer.observe(html, {childList: true, subtree: false});
+    }
+
+    const prompt = document.createElement("div");
+    prompt.id = "login-prompt";
+    html.appendChild(prompt);
+
+    const xButton = document.createElement("button");
+    xButton.id = "x-button";
+    xButton.innerText = "X";
+    xButton.onclick = () => {
+        film.remove();
+        prompt.remove();
+    }
+    prompt.appendChild(xButton);
+
+    const title = document.createElement("p");
+    title.innerText = "User Login";
+    prompt.appendChild(title);
+
+    const form = document.createElement("form");
+    prompt.appendChild(form);
+
+    let inputRow = document.createElement("div");
+    inputRow.className = "input-row";
+    const usernameInput = document.createElement("input");
+    usernameInput.type = "text";
+    usernameInput.id = "username-input";
+    usernameInput.name = "username";
+    usernameInput.placeholder = "Username: ";
+    inputRow.appendChild(usernameInput);
+    form.appendChild(inputRow);
+
+    inputRow = document.createElement("div");
+    inputRow.className = "input-row";
+    const passwordInput = document.createElement("input");
+    passwordInput.type = "password";
+    passwordInput.id = "password-input";
+    passwordInput.name = "password";
+    passwordInput.placeholder = "Password: ";
+    inputRow.appendChild(passwordInput);
+    form.appendChild(inputRow);
+
+    inputRow = document.createElement("div");
+    inputRow.className = "input-row";
+    const button = document.createElement("input");
+    button.type = "button";
+    button.name = "submitbutton";
+    button.value = "Log In";
+    button.onclick = signin;
+    inputRow.appendChild(button);
+    form.appendChild(inputRow);
+
+    const signup = document.createElement("p");
+    signup.innerText = "Don't have an account? ";
+    const signupLink = document.createElement("a");
+    signupLink.innerText = "Sign Up!";
+    signupLink.href = "/signup";
+    signup.appendChild(signupLink);
+    prompt.appendChild(signup);
+}
+
+async function signin(clickEvent) {
+    const form = clickEvent.explicitOriginalTarget.form;
+    const destination = `${API_URL}:${API_PORT}/login`;
+
+    const data = {
+        username: form.username.value,
+        password: form.password.value
+    };
+
+    try {
+        const response = await fetch(destination, {
+            method: 'POST',
+            redirect: 'manual',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+            },
+            referrerPolicy: 'no-referrer',
+            body: new URLSearchParams(data),
+            credentials: 'include'
+        });
+        
+        if (response.ok) {
+            window.location.href = '/profile';
+        } else {
+            //TODO: handle incorrect logins
+        }
+    } catch (error) {
+        //render something to indicate an error, not incorrect login
+    }
 }
 
 function makeHeader(name) {
@@ -54,8 +171,8 @@ function makeHeader(name) {
         signOut.innerText = "Sign Out";
         dropdown.appendChild(signOut);
     } else {
-        elem = document.createElement("a");
-        elem.href = "/signin";
+        elem = document.createElement("button");
+        elem.onclick = makeSigninPrompt;
         elem.innerText = "Sign In";
     }
     elem.className = "navigation";
